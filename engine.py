ad146fcf0938b35c5cd8933c25d61e48ba3dc739
PropFeds <propfeds@gmail.com>
2019-07-31 15:11:39 +0700

    Font Consolas 10px

--- engine.py
+++ engine.py
@@ -1,7 +1,11 @@
 import tcod
 
 def main():
-    print('Hullo Snek!')
+    terminal_height=50
+    terminal_width=80
+
+    tcod.console_set_custom_font('consolas_unicode_10x10.png', tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_TCOD)
+    
 
 if __name__=='__main__':
     main()
