554b23748765c5a93b857579d4379ab102a59b21
PropFeds <propfeds@gmail.com>
2019-07-31 23:21:36 +0700

    Factions enum

---
+++ factions.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class Factions(Enum):
+    NEUTRAL=0
+    ALLY=1
+    ENEMY=2
--- game_states.py
+++ game_states.py
@@ -3,9 +3,9 @@ from enum import Enum
 class GameStates(Enum):
     DEATH=0         # Faction Departed
     PLAYER_TURN=1   # Faction Good
-    ALLY_TURN=2     # Faction Good (your fellow snekmen and charmed sneks)
+    ALLY_TURN=2     # Faction Good (your kin and charmed animals)
     ENEMY_TURN=3    # Faction Bad (humans)
-    NEUTRAL_TURN=4  # Faction Neutral (traps, critters, some human NPCs, aggressiveness depends on AI type)
+    NEUTRAL_TURN=4  # Faction Neutral (traps, critters)
 
     MENU=10         # Supposed to be options?
     INVENTORY=11    # Press inventory then press key to pull up the item menu with options such as [drop, wield, use]
