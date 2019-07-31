3eb86ad819d7a590cd0acfb37194baf55cf5ac73
PropFeds <propfeds@gmail.com>
2019-07-31 22:53:50 +0700

    New tile attributes: char, colour (like entities)

--- map_objects/tile.py
+++ map_objects/tile.py
@@ -1,6 +1,8 @@
 class Tile:
-    def __init__(self, name, pathable, transparent):
+    def __init__(self, name, char, colour, pathable, transparent):
         self.name=name
+        self.char=char
+        self.colour=colour
         self.pathable=pathable
         self.transparent=transparent
         self.explored=False
