edd9005bca01d5922ffd35fe87eab8a41551aedc
PropFeds <propfeds@gmail.com>
2019-07-31 15:41:45 +0700

    config.json and an empty terminal

--- engine.py
+++ engine.py
@@ -1,10 +1,23 @@
 import tcod
+import json
 
 def main():
-    terminal_height=50
-    terminal_width=80
+    # Importing data from config.json
+    with open('config.json') as cfg:
+        data=json.load(cfg)
+    terminal_width=data['terminal_width']
+    terminal_height=data['terminal_height']
+    map_width=data['map_width']
+    map_height=data['map_height']
+    max_monsters_per_room=data['max_monsters_per_room']
+    max_items_per_room=data['max_items_per_room']
+    fov_algorithm=data['fov_algorithm']
+    fov_light_walls=bool(data['fov_light_walls'])
+    fov_radius=data['fov_radius']
 
+    # Init console
     tcod.console_set_custom_font('consolas_unicode_10x10.png', tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_TCOD)
+    tcod.console_init_root(terminal_width, terminal_height, 'Dance of the Pythons', fullscreen=False)
     
 
 if __name__=='__main__':
