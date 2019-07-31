4a5cb06acb9794ab533bbf43c49c5c0df971a0c9
PropFeds <propfeds@gmail.com>
2019-08-01 02:34:56 +0700

    New game state

--- game_states.py
+++ game_states.py
@@ -1,12 +1,14 @@
 from enum import Enum
 
 class GameStates(Enum):
-    DEATH=0         # Faction Departed
-    PLAYER_TURN=1   # Faction Good
-    ALLY_TURN=2     # Faction Good (your kin and charmed animals)
-    ENEMY_TURN=3    # Faction Bad (humans)
-    NEUTRAL_TURN=4  # Faction Neutral (traps, critters)
+    DEATH=0         # Welcome to the Hollows
+    PLAYER_TURN=1   # Team Good
+    ALLY_TURN=2     # Team Good (your kin and charmed animals)
+    ENEMY_TURN=3    # Team Bad (humans)
+    NEUTRAL_TURN=4  # Team Neutral (traps, critters)
 
-    MENU=10         # Supposed to be options?
-    INVENTORY=11    # Press inventory then press key to pull up the item menu with options such as [drop, wield, use]
-    TARGETING=12    # The cursed thing, but now with keyboard controls instead
+    MENU=10             # Supposed to be options?
+    INVENTORY=11        # Press inventory then press key to pull up the item menu with options such as [drop, wield, use]
+    EQUIPMENT=12        # You know where you wear that shiny plate mail
+    POINT_TARGETING=13  # The cursed thing, but now with keyboard controls instead
+    DIR_TARGETING=14    # Nine directions which if self-target (NumPad5 or s) fires to ground
