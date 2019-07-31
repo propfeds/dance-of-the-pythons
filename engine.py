06458ba91f2a0b7150ef5eaa66ad6600c066caf1
PropFeds <propfeds@gmail.com>
2019-08-01 01:30:23 +0700

    More inits

--- engine.py
+++ engine.py
@@ -1,6 +1,10 @@
 import tcod
 import tcod.event
 import json
+from map_objects.game_map import GameMap
+from components.inventory import Inventory
+from entity import Entity
+from render_functions import RenderOrder
 
 def main():
     # Importing data from data/config.json
@@ -14,11 +18,14 @@ def main():
     fov_light_walls=bool(data['fov_light_walls'])
     fov_radius=data['fov_radius']
 
-    # Init console
+    # Init console, player and stuff
     tcod.console_set_custom_font('data/consolas_unicode_10x10.png', tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_TCOD)
-    tcod.console_init_root(terminal_width, terminal_height, 'Dance of the Pythons', False, tcod.RENDERER_SDL2, 'F', False)
-
-
+    tcod.console_init_root(terminal_width, terminal_height, 'Dance of the Pythons', False, tcod.RENDERER_SDL2, 'C', False)
+    player=Entity(25, 25, '@', tcod.yellow, 'Ratiel Snailface the Enchanter', 5, 1, 0, RenderOrder.ACTOR, False, None, None, None, Inventory(26))
+    entities=[player]
+    display=tcod.console.Console(terminal_width, terminal_height, 'C')  # C not for Celsius
+    interface=tcod.console.Console(terminal_width, map_height, 'C')
+    game_map=GameMap(map_width, map_height)
     # Game loop
     while True:
         for event in tcod.event.wait():
