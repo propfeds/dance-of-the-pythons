636de6da504a2b5304a231d4f5cb3480ccbf46c8
PropFeds <propfeds@gmail.com>
2019-08-02 23:47:12 +0700

    Removed member name from Tile class

--- map_objects/tile.py
+++ map_objects/tile.py
@@ -1,6 +1,5 @@
 class Tile:
-    def __init__(self, char, lit_colour, dim_colour, name):
+    def __init__(self, char, lit_colour, dim_colour):
         self.char=char
         self.lit_colour=lit_colour
         self.dim_colour=dim_colour
-        self.name=name
