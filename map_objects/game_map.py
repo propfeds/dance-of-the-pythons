dbd78aa872b9de26d0d2d55cde696014c1b5d7b5
PropFeds <propfeds@gmail.com>
2019-08-03 17:44:48 +0700

    More dirt path testing

--- map_objects/game_map.py
+++ map_objects/game_map.py
@@ -16,7 +16,11 @@ class GameMap:
         self.graphics_map=numpy.full((height, width), Tile('.', (106, 190, 48), (57, 60, 50)))
         self.explored=numpy.full((height, width), False)
         origin_x=3
-        dest_x=generator.cave_y(origin_x, 0, 21, Tile('•', (138, 111, 48), (82, 75, 36)), 60, 60, 1, 1, 3, self)
+        # Dirt paths
+        dest_x=generator.cave_y(origin_x, 0, 27, Tile('•', (138, 111, 48), (82, 75, 36)), 50, 50, 1, 1, 3, self)
+        dest_x=generator.cave_y(origin_x, 0, 27, Tile('•', (138, 111, 48), (82, 75, 36)), 100, 100, 1, 0, 2, self)
+        # Dirt walls
+        
 
     def recompute_fov(self, x, y, radius, light_walls, algorithm):
         self.path_map.compute_fov(x, y, radius, light_walls, algorithm)
