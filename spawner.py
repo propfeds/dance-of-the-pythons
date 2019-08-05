34ffc419f8835177a9d5d851005cd2d65ce78feb
PropFeds <propfeds@gmail.com>
2019-08-05 18:34:36 +0700

    Spawner now returns some weird json structure

--- spawner.py
+++ spawner.py
@@ -29,13 +29,13 @@ class Spawner:
     def check_collision(self, x, y):
         # Colliding with the offworld
         if x<0 or y<0 or x>=self.width or y>=self.height:
-            return True
+            return {'outofbounds': True}
         if not self.path_map.walkable[y, x]:
-            return True
+            return {'blocked': True}
         for entity in self.entities:
             if entity.x==x and entity.y==y:
-                return True
-        return False
+                return {'collide': entity}
+        return {}
 
     def spawn_actor(self, x, y, entity_name, faction):
         if self.check_collision(x, y):
@@ -45,6 +45,7 @@ class Spawner:
             short=self.entity_data['actors'][entity_name]['walkable']
             entity=Entity(x, y, entity_name, faction, self.entity_gfx['actors'][entity_name]['char'], tuple(self.palette[self.entity_gfx['actors'][entity_name]['colour']]), self.entity_data['actors'][entity_name]['hp_max'], self.entity_data['actors'][entity_name]['attack'], self.entity_data['actors'][entity_name]['shield'], self.entity_data['actors'][entity_name]['alert_threshold'], (RenderOrder.ACTOR_SHORT if short else RenderOrder.ACTOR), short, Inventory(self.entity_data['actors'][entity_name]['inventory_capacity']), (None if (entity_name=='player') else get_ai(self.entity_data['actors'][entity_name]['ai'])))
             self.entities.append(entity)
+            self.path_map.walkable[y, x]=short
             return {'spawned': True}
 
     def spawn_furniture(self, x, y, entity_name):
