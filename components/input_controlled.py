1589639d1579f55523cc5d5d9b98e54bd5ccf030
PropFeds <propfeds@gmail.com>
2019-09-12 17:57:11 +0700

    InputControlled component: Player and Possesseds

---
+++ components/input_controlled.py
@@ -0,0 +1,4 @@
+class InputControlled:
+    def __init__(self, parent_entity=None):
+        # The player, as well as any possessed entity
+        self.parent_entity=parent_entity
--- components/movement.py
+++ components/movement.py
@@ -1,8 +1,9 @@
 class Movement:
-    def __init__(self, x, y, walkable=True, speed=100, hover=False):
+    def __init__(self, x, y, walkable, swappable=False, speed=100, hover=False):
        self.x=x
        self.y=y
        self.walkable=walkable
+       self.swappable=swappable
        self.speed=speed
        # Hover: a bool indicating hovering status
        self.hover=hover
