6dd70f4e25a94e8e71dbb6665b35e717f348d853
PropFeds <propfeds@gmail.com>
2019-09-12 18:07:08 +0700

    Defence component: to_hit (modifier) member

--- components/defence.py
+++ components/defence.py
@@ -1,5 +1,7 @@
 class Defence:
-    def __init__(self, max_health, ac=0):
+    def __init__(self, max_health, ac=0, to_hit=0):
         self.max_health=max_health
         self.health=max_health
         self.ac=ac
+        # To hit is a bonus
+        self.to_hit=to_hit
