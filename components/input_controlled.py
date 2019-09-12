24f8467dcdcb173c6db1aec4a59b56c65ab8f8af
PropFeds <propfeds@gmail.com>
2019-09-12 18:07:25 +0700

    Separating InputControlled and Possessed components

--- components/input_controlled.py
+++ components/input_controlled.py
@@ -1,4 +1,3 @@
 class InputControlled:
-    def __init__(self, parent_entity=None):
-        # The player, as well as any possessed entity
-        self.parent_entity=parent_entity
+    def __init__(self):
+        pass
---
+++ components/possessed.py
@@ -0,0 +1,3 @@
+class Possessed:
+    def __init__(self, parent_entity):
+        self.parent_entity=parent_entity
