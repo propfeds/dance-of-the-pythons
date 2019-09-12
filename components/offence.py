7cbbceae363a76733d01d2e57198ec4fa4f34a64
PropFeds <propfeds@gmail.com>
2019-09-12 18:06:43 +0700

    Offence component: piercing, lethal members

--- components/offence.py
+++ components/offence.py
@@ -1,5 +1,8 @@
 class Offence:
-    def __init__(self, damage, to_hit=0):
+    def __init__(self, damage, to_hit=0, piercing=False, lethal=True):
         # To hit is a bonus
         self.damage=damage
         self.to_hit=to_hit
+        # Piercing means to penetrate overshield
+        self.piercing=piercing
+        self.lethal=lethal
