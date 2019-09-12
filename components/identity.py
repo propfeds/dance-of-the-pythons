6aaed695150544c4d110b9a47ab0d5cb4f1f670d
PropFeds <propfeds@gmail.com>
2019-09-12 18:19:29 +0700

    Separated Faction and Identity components

--- components/identity.py
+++ components/identity.py
@@ -1,9 +1,8 @@
 from data.enums import Factions
 
 class Identity:
-    def __init__(self, name, given_name=None, faction=None):
+    def __init__(self, name, given_name=None):
         # Name is monster type: Snake, Soldier, etc.
         # Given name is for fellow tribesmen or plot characters
         self.name=name
         self.given_name=given_name
-        self.faction=faction
