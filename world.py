24596333176e128c1440dca038065a92c9e1f71d
PropFeds <propfeds@gmail.com>
2019-09-13 18:46:36 +0700

    Level arguments for Spawner and TerrainMap

--- world.py
+++ world.py
@@ -5,5 +5,5 @@ import systems
 class World:
     def __init__(self):
         self.renderer=systems.renderer.Renderer()
-        self.spawner=systems.spawner.Spawner()
-        self.terrain_map=systems.terrain_map.TerrainMap(self.renderer.config['consoles']['width'][0], self.renderer.config['consoles']['height'][0])
+        self.spawner=systems.spawner.Spawner(0)
+        self.terrain_map=systems.terrain_map.TerrainMap(self.renderer.config['consoles']['width'][0], self.renderer.config['consoles']['height'][0], 0)
