1f1f9dac8254d970c92fd999716523f02f55ade4
PropFeds <propfeds@gmail.com>
2019-11-09 22:40:45 +0700

    Added properties and changed dice string format

--- components/die.py
+++ components/die.py
@@ -5,21 +5,32 @@ class Die:
         self.dice=dice
         self.sides=sides
         self.mod=mod
-    
+
     def roll(self):
         result=self.mod
         for _ in range(self.dice):
             result+=randint(1, self.sides)
         return result
-    
-    def get_min(self):
+
+    @property
+    def roll_min(self):
         return self.dice+self.mod
-    
-    def get_max(self):
+
+    @property
+    def roll_max(self):
         return self.dice*self.sides+self.mod
-    
-    def get_average(self):
-        return ((float(self.get_min())+float(self.get_max()))/2.0)
+
+    @property
+    def roll_average(self):
+        return ((float(self.roll_min)+float(self.roll_max))/2.0)
 
     def __str__(self):
-        return f'{self.dice}d{self.sides}+{self.mod}'
+        die_format=''
+        if self.dice!=1:
+            die_format+=str(self.dice)
+        die_format+='d{0}'.format(self.sides)
+        if self.mod!=0:
+            if self.mod>0:
+                die_format+='+'
+            die_format+=str(self.mod)
+        return die_format
